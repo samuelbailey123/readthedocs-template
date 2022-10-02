bd575edb951f780c3618c8db9aee9e336aa66dee
Samuel Bailey <baileysam1997@gmail.com>
2022-10-02 17:15:08 +0200

    added css static

--- conf.py
+++ conf.py
@@ -17,19 +17,31 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "myst_parser",
+    "sphinx_markdown_tables",
+    "sphinx_tabs.tabs",
+    "sphinxcontrib.httpdomain",
+    "sphinxext.opengraph"
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_title = "Fast API"
+html_theme_options = {
+    "display_version": False,
+    "prev_next_buttons_location": False,
+    "sticky_navigation": False
+}
+html_css_files = [
+    "/_static/custom.css?" + str(round(time.time())),
+    "https://use.fontawesome.com/releases/v5.13.0/css/all.css"
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
