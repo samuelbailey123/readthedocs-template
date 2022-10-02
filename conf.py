be56b4db4c69c83c9ffe696d3892eeff90e149d3
Samuel Bailey <baileysam1997@gmail.com>
2022-10-02 17:18:32 +0200

    Added custom js

--- conf.py
+++ conf.py
@@ -1,22 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
+import time
 
-# -- Project information
-
-project = 'Fast API'
-copyright = 'Samuel Bailey'
-author = 'Samuel Bailey'
-
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
+exclude_patterns = ["README.md"]
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     "myst_parser",
     "sphinx_markdown_tables",
     "sphinx_tabs.tabs",
@@ -24,24 +10,21 @@ extensions = [
     "sphinxext.opengraph"
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-html_title = "Fast API"
+html_css_files = [
+    "/_static/custom.css?" + str(round(time.time())),
+    "https://use.fontawesome.com/releases/v5.13.0/css/all.css"
+]
+html_js_files = ["/_static/custom.js?" + str(round(time.time()))]
+html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "display_version": False,
     "prev_next_buttons_location": False,
     "sticky_navigation": False
 }
-html_css_files = [
-    "/_static/custom.css?" + str(round(time.time())),
-    "https://use.fontawesome.com/releases/v5.13.0/css/all.css"
-]
+html_title = "Fast API"
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+myst_heading_anchors = 6
+
+project = "Fast API"
+author = "Samuel Bailey"
